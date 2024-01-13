6ce14820b75035f0713cbaf718c2415d79653c2b
aquilisalvesdemelooliveira@gmail.com <aquilisalvesdemelooliveira@gmail.com>
2024-01-13 18:13:41 -0300

    organizei em arquivos

---
+++ java/Banco/Banco.java
@@ -0,0 +1,17 @@
+public class Banco{
+
+    public static void main(String[] args) {
+
+        Conta minhaConta = new Conta();
+
+        System.out.println(minhaConta.getDadosDaConta());
+
+        minhaConta.depositar(100);
+        minhaConta.sacar(50);
+
+        System.out.println(minhaConta.getDadosDaConta());
+
+    }
+
+}
+
